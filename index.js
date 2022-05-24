2456555f5375ba883eb903cda6b59c9c663b4faa
greymoon <aljog786@gmail.com>
2022-05-24 14:48:59 +0530

    without mongoDB

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const path=require('path');
+
+const app= express();
+const ejs=require('ejs');
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'pages/index.html'));
+    res.render('index');
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/post',(req,res)=>{
+    res.render('post');
+})
+
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
+
+app.listen(4000,()=>{
+    console.log('App listening on port,4000');
+})
